798c9bdefe32064e890046a598286f508acc28ef
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-10 23:19:13 +0900

    Initial Commit

---
+++ NameScoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NameScoreApp
+//
+//  Created by ginger893 on 2021/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
